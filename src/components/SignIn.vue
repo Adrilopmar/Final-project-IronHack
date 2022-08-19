5b2beb0e56557da05202eb07767364a1b2d72218
Adrià <adria.lop.mar@gmail.com>
2022-08-19 12:49:22 +0200

    sign in & sign up components. styles missing

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,6 +1,46 @@
 <template>
-  <div>Sign In</div>
-  <PersonalRouter :route="route" :buttonText="buttonText" />
+<div class="sign-in-bg m-auto">
+  <h3 class="text-4xl font-semibold mb-9">Sign In</h3>
+  <form class="w-full max-w-sm">
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-full-name">
+        Email
+      </label>
+    </div>
+    <div class="md:w-2/3">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-cyan-500" id="inline-full-name" type="text" value="example@mail.com">
+    </div>
+  </div>
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-password">
+        Password
+      </label>
+    </div>
+    <div class="md:w-2/3">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************">
+    </div>
+  </div>
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3"></div>
+    <label class="md:w-2/3 block text-gray-500 font-bold">
+      
+      <span class="text-sm">Not having an account yet?</span>
+      <br>
+      <span class="text-xl"><PersonalRouter :route="route" :buttonText="buttonText" /></span>
+    </label>
+  </div>
+  <div class="md:flex md:items-center">
+    <div class="md:w-1/3"></div>
+    <div class="md:w-2/3">
+      <button class="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+        Sign Up
+      </button>
+    </div>
+  </div>
+</form>
+</div>
 </template>
 
 <script setup>
@@ -13,7 +53,7 @@ import { storeToRefs } from "pinia";
 
 // Route Variables
 const route = "/auth/sign-up";
-const buttonText = "Test the Sign Up Route";
+const buttonText = "Register!";
 
 // Input Fields
 const email = ref("");
@@ -50,18 +90,18 @@ const signIn = async () => {
 </script>
 
 <style socped>
-.wu-text {
-  color: black;
+.sign-in-bg{
+  padding: 3rem;
+  border-radius: 25px;
+  background: #add2ff;
+  
 }
 
-.form {
-  display: flex;
-  flex-direction: column;
-  margin: 1rem 0;
-}
-.input {
-  color: black;
-  margin-bottom: 1rem;
+
+form {
+
+ 
+
 }
 .button {
   background-color: #4caf50; /* Green */
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,6 +1,76 @@
 <template>
-  <div>Sign Up</div>
-  <PersonalRouter :route="route" :buttonText="buttonText" />
+  <div class="sign-up-bg m-auto">
+  <h3 class="text-4xl font-semibold mb-9">Register</h3>
+  <form class="w-full max-w-sm">
+    <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-full-name">
+        Name
+      </label>
+    </div>
+     <div class="md:w-2/3">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-cyan-500" id="inline-full-name" type="text" value="example@mail.com">
+    </div>
+  </div>
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-full-last-name">
+        Last name
+      </label>
+    </div>
+     <div class="md:w-2/3">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-cyan-500" id="inline-full-last-name" type="text" value="example@mail.com">
+    </div>
+  </div>
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-full-email">
+        Email
+      </label>
+    </div>
+    <div class="md:w-2/3">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-cyan-500" id="inline-full-email" type="email" value="example@mail.com">
+    </div>
+  </div>
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-password">
+        Password
+      </label>
+    </div>
+    <div class="md:w-2/3">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************">
+    </div>
+  </div>
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-confirm-password">
+       Confirm password
+      </label>
+    </div>
+    <div class="md:w-2/3">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-confirm-password" type="password" placeholder="******************">
+    </div>
+  </div>
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3"></div>
+    <label class="md:w-2/3 block text-gray-500 font-bold">
+      
+      <span class="text-sm">Alredy registered?</span>
+      <br>
+      <span class="text-xl"><PersonalRouter :route="route" :buttonText="buttonText" /></span>
+    </label>
+  </div>
+  <div class="md:flex md:items-center">
+    <div class="md:w-1/3"></div>
+    <div class="md:w-2/3">
+      <button class="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+        Sign Up
+      </button>
+    </div>
+  </div>
+</form>
+</div>
 </template>
 
 <script setup>
@@ -8,7 +78,7 @@ import PersonalRouter from "./PersonalRouter.vue";
 
 // Route Variables
 const route = "/auth/login";
-const buttonText = "Test the Sign In Route";
+const buttonText = "Sing in";
 
 // Input Fields
 
@@ -23,4 +93,11 @@ const buttonText = "Test the Sign In Route";
 // Arrow function to SignUp user to supaBase with a timeOut() method for showing the error
 </script>
 
-<style></style>
+<style scope>
+.sign-up-bg{
+  padding: 3rem;
+  border-radius: 25px;
+  background: #add2ff;
+  
+}
+</style>
