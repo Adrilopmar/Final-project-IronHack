623045ce50702533c02c12fb66de3a4c29ca4834
Adrià <adria.lop.mar@gmail.com>
2022-09-26 16:25:10 +0200

    removed username from sing up form

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,32 +1,7 @@
 <template>
-  <navNotLogged class="mb-12" />
   <div class="sign-up-bg m-auto">
     <h3 class="text-4xl font-semibold mb-9">Register</h3>
     <form class="w-full max-w-sm">
-      <!-- Name input -->
-      <div class="md:flex md:items-center mb-6">
-        <div class="md:w-1/3">
-          <label
-            class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
-            for="inlineUserName"
-          >
-            Username
-          </label>
-        </div>
-        <div class="md:w-2/3">
-          <input
-            v-model="userName"
-            @focus="toogleError('name')"
-            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-cyan-500"
-            id="inlineUserName"
-            type="text"
-            placeholder="Name"
-          />
-          <p v-if="showNameError" class="error text-center text-xs">
-            Name must be 4 characters long
-          </p>
-        </div>
-      </div>
       <!-- Email input -->
       <div class="md:flex md:items-center mb-6">
         <div class="md:w-1/3">
